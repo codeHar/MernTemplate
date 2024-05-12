2e6beb26b43647620915f728bd39aea95b07288f
codeHar <amit723shrestha@gmail.com>
2024-05-12 12:56:12 +0545

    inital commit

---
+++ frontend/src/pages/About.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const About = () => {
+  return (
+    <div>
+      About
+      <Link to={"/"}>Home page</Link>
+    </div>
+  );
+};
+
+export default About;
